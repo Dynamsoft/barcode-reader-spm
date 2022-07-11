f43a2323afd8f400f739dfe1a5d2d5a4a6bc208a
arckongem <arckongem@gmail.com>
2022-07-11 16:50:38 +0800

    9.2.11

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DynamsoftBarcodeReader",
-            url: "https://download.dynamsoft.com/xcframeworks/dynamsoft-barcodereader-ios-9.2.10-xcframework.zip",
-            checksum: "037b83ff55d6678c71b44c993f40ded3f15d79410877700f3744a6c0dee3bdfe"
+            url: "https://download.dynamsoft.com/xcframeworks/dynamsoft-barcodereader-ios-9.2.11-xcframework.zip",
+            checksum: "3afeeef5c5e9c7b276436d130d5c1377a66247d677f4272e9ca8427f7036c026"
         ),
     ]
 )
