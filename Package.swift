f2dd18b56950742e6e04984a8edcf7113aadfe01
DMGithubPublisher <github@dynamsoft.com>
2024-11-13 14:34:02 +0800

    update to 10.4.2001

--- Package.swift
+++ Package.swift
@@ -24,6 +24,6 @@ let package = Package(
         .binaryTarget(name: "DynamsoftImageProcessing", url: "https://download2.dynamsoft.com/xcframeworks/dynamsoft-imageprocessing-ios-2.4.20-xcframework.zip", checksum: "f1d325c61e2de175c00dfd566c0a1ac714c50a13fea4812ca786a9027dd512bb"),
         .binaryTarget(name: "DynamsoftBarcodeReader", url: "https://download2.dynamsoft.com/xcframeworks/dynamsoft-barcodereader-ios-10.4.20-xcframework.zip", checksum: "dd8a0d12ba5f2f7339f91870faec7ca8a8c9779b8da93488cab0e1c0d31ef5f4"),
         .binaryTarget(name: "DynamsoftUtility", url: "https://download2.dynamsoft.com/xcframeworks/dynamsoft-utility-ios-1.4.20-xcframework.zip", checksum: "47b913f26da1f3416be924fc9fd70f8779722a551d2d191307c1a885c447c4c2"),
-        .binaryTarget(name: "DynamsoftCaptureVisionRouter", url: "https://download2.dynamsoft.com/xcframeworks/dynamsoft-capturevisionrouter-ios-2.4.20-xcframework.zip", checksum: "8abd53ea9d0b0c4612bb19d3f8ec0c4f87eef3ad9f57fdaad7fad58a2bb7143f"),
+        .binaryTarget(name: "DynamsoftCaptureVisionRouter", url: "https://download2.dynamsoft.com/xcframeworks/dynamsoft-capturevisionrouter-ios-2.4.21-xcframework.zip", checksum: "9e44d32e5a037c977f9d3bf2edbdf81917d09d7392ee6b8c3c40e6203f7d8f97"),
     ]
 )
