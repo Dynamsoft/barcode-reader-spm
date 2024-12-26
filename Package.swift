6ec5304c074c5ab6f935dd95a1683948ca073c7a
DMGithubPublisher <github@dynamsoft.com>
2024-12-26 13:49:33 +0800

    update to 10.4.2003

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
         .library(name: "DynamsoftBarcodeReader", targets: ["DynamsoftBarcodeReader"]),
         .library(name: "DynamsoftUtility", targets: ["DynamsoftUtility"]),
         .library(name: "DynamsoftCaptureVisionRouter", targets: ["DynamsoftCaptureVisionRouter"]),
+        .library(name: "DynamsoftBarcodeReaderBundle", targets: ["DynamsoftBarcodeReaderBundle"]),
     ],
     dependencies: [
     ],
@@ -25,5 +26,6 @@ let package = Package(
         .binaryTarget(name: "DynamsoftBarcodeReader", url: "https://download2.dynamsoft.com/xcframeworks/dynamsoft-barcodereader-ios-10.4.20-xcframework.zip", checksum: "dd8a0d12ba5f2f7339f91870faec7ca8a8c9779b8da93488cab0e1c0d31ef5f4"),
         .binaryTarget(name: "DynamsoftUtility", url: "https://download2.dynamsoft.com/xcframeworks/dynamsoft-utility-ios-1.4.20-xcframework.zip", checksum: "47b913f26da1f3416be924fc9fd70f8779722a551d2d191307c1a885c447c4c2"),
         .binaryTarget(name: "DynamsoftCaptureVisionRouter", url: "https://download2.dynamsoft.com/xcframeworks/dynamsoft-capturevisionrouter-ios-2.4.21-xcframework.zip", checksum: "9e44d32e5a037c977f9d3bf2edbdf81917d09d7392ee6b8c3c40e6203f7d8f97"),
+        .binaryTarget(name: "DynamsoftBarcodeReaderBundle", url: "https://download2.dynamsoft.com/xcframeworks/dynamsoft-barcodereader-bundle-ios-10.4.2003-xcframework.zip", checksum: "6079a01d638b048addb3f6e40ee1bdcb387af25dc66c6fa6cca578464ef377f1"),
     ]
 )
