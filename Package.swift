62d056cc31ce13e2194f1978967897a784271dcb
DMGithubPublisher <github@dynamsoft.com>
2025-11-05 15:17:53 +0800

    update to 11.2.3000

--- Package.swift
+++ Package.swift
@@ -10,13 +10,13 @@ let package = Package(
         .library(name: "DynamsoftBarcodeReader", targets: ["DynamsoftBarcodeReader"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Dynamsoft/capture-vision-spm.git", from: "3.2.1000")
+        .package(url: "https://github.com/Dynamsoft/capture-vision-spm.git", from: "3.2.3000")
     ],
     targets: [
         .binaryTarget(
             name: "DynamsoftBarcodeReaderBundle", 
-            url: "https://download2.dynamsoft.com/xcframeworks/dynamsoft-barcodereader-bundle-ios-11.2.1000-xcframework.zip", 
-            checksum: "c24a91a087746b09154abadbd759cabf4e902be74a1c974fe91339a3612e8a9f"
+            url: "https://download2.dynamsoft.com/xcframeworks/dynamsoft-barcodereader-bundle-ios-11.2.3000-xcframework.zip", 
+            checksum: "2120289e29f419f2512ba1fdb9f09f13965d7610e73651765af8bd63657bcf3d"
         ),
         .target(
             name: "DynamsoftBarcodeReader",
