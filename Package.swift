f71d9e54652a451b62f230742d2d903f6a2c6b03
DMGithubPublisher <github@dynamsoft.com>
2025-12-23 14:34:46 +0800

    update to 9.6.60

--- Package.swift
+++ Package.swift
@@ -1,29 +1,22 @@
 // swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "DynamsoftBarcodeReader",
-    platforms: [.iOS(.v13)],
-    products: [
-        .library(name: "DynamsoftBarcodeReader", targets: ["DynamsoftBarcodeReader"]),
+    platforms: [
+        .iOS(.v9),
     ],
-    dependencies: [
-        .package(url: "https://github.com/Dynamsoft/capture-vision-spm.git", from: "3.2.5000")
+    products: [
+        .library(
+            name: "DynamsoftBarcodeReader",
+            targets: ["DynamsoftBarcodeReader"]),
     ],
     targets: [
         .binaryTarget(
-            name: "DynamsoftBarcodeReaderBundle", 
-            url: "https://download2.dynamsoft.com/xcframeworks/dynamsoft-barcodereader-bundle-ios-11.2.5000-xcframework.zip", 
-            checksum: "4432b7d5c2431122907cda24773536b7f5ff36202a9fe678fbc4614798b843c2"
-        ),
-        .target(
             name: "DynamsoftBarcodeReader",
-            dependencies: [
-                "DynamsoftBarcodeReaderBundle",
-                .product(name: "DynamsoftCaptureVisionBundle", package: "capture-vision-spm")
-            ]
-        )
+            url: "https://download2.dynamsoft.com/xcframeworks/dynamsoft-barcodereader-ios-9.6.60-xcframework.zip",
+            checksum: "72998f2863404035cad4e724056327d40e082b3e70c954d3625665d376bd35dc"
+        ),
     ]
 )
